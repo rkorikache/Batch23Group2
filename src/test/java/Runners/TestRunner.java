69e19d3d3d8dab6a194b2d401f085aad5ae43ec8
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-08-27 20:54:37 -0400

    done

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -1,4 +1,25 @@
-package Runners;
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //path of the feature directory or feature file
+        features = "src/test/resources/features/",
+        //name of the package where all steps are found
+        glue = "steps",
+        //when we set the value of dry run to true, it stops execution and generate all missing step definition
+        //when we have to execute, we set the value to false
+        dryRun = false,
+        tags = "@params"
+        //pretty plugin prints all the steps in console
+        //plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json","rerun:target/failed.txt"}
+)
+
 
 public class TestRunner {
 }
